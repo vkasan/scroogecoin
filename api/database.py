0a36a92d4fe721791fbc073fbc4882a710ef149c
Вадим <vkasanlive@gmail.com>
2024-04-17 13:24:33 +0700

    Базовая модель пользователя

---
+++ api/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+databaseUrl = 'sqlite:///./scroogecoin.db'
+
+
+engine = create_engine(databaseUrl, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(320), unique=True)
+    pwd_hash = Column(String(64))
+    user_name = Column(String(50))
