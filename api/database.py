d03bdd9ee36e787383980b232949188a7a7152f6
Вадим <vkasanlive@gmail.com>
2024-05-15 09:54:17 +0700

    Добавление новых моделей запросов и сущностей БД

--- api/database.py
+++ api/database.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, Float, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 databaseUrl = 'sqlite:///./scroogecoin.db'
 databaseCreateOnStartup = True
@@ -25,6 +25,24 @@ class User(Base):
     user_name = Column(String(50))
     email = Column(String(320), unique=True)
     pwd_hash = Column(String(64))
+
+
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100))
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", backref="categories")
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    amount = Column(Float)
+    date = Column(DateTime)
+    description = Column(String)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("Category", backref="transactions")
 
 
 if databaseCreateOnStartup:
--- api/json_models.py
+++ api/json_models.py
@@ -10,3 +10,60 @@ class RegisterRequest(BaseModel):
     email: str
     pwd: str
 
+
+class UserInfo(BaseModel):
+    id: int
+    name: str
+    email: str
+
+
+class CategoryJson(BaseModel):
+    id: int = 0
+    name: str
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class TransactionJson(BaseModel):
+    id: int = 0
+    amount: float
+    date: str
+    description: str
+    category_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class CategoriesResponse(BaseModel):
+    categories: list[CategoryJson] = []
+
+
+class TransactionsResponse(BaseModel):
+    transactions: list[TransactionJson] = []
+
+
+class UsersResponse(BaseModel):
+    users: list[UserInfo] = []
+
+
+class CategoryCreateRequest(BaseModel):
+    owner_id: int
+    name: str
+
+
+class TransactionCreateRequest(BaseModel):
+    category_id: int
+    amount: float
+    date: str
+    description: str
+
+
+class IdJson(BaseModel):
+    id: int
+
+
+class UserUpdateProfileRequest(BaseModel):
+    name: str | None = None
