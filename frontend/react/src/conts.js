0f85f106eb6db4faad18c53f3d57d3cdf9544eeb
Valeriya132000 <92806400+Valeriya132000@users.noreply.github.com>
2024-05-24 12:38:12 +0000

    Запросы на категории, и исторую расходов и доходов

---
+++ frontend/react/src/conts.js
@@ -0,0 +1,38 @@
+
+export const catygoryIcons = [
+  {
+    name: 'cart',
+    icon: '/img/category-icons/icon-cart.svg',
+  },
+  {
+    name: 'car',
+    icon: '/img/category-icons/icon-car.svg',
+  },
+  {
+    name: 'health',
+    icon: '/img/category-icons/icon-health.svg',
+  },
+  {
+    name: 'clothes',
+    icon: '/img/category-icons/icon-clothes.svg',
+  },
+  {
+    name: 'fun',
+    icon: '/img/category-icons/icon-fun.svg',
+  },
+  {
+    name: 'home',
+    icon: '/img/category-icons/icon-home.svg',
+  }
+]
+
+export const categoryIcon = {
+  'default': '/img/category-icons/icon-income.svg',
+  'income': '/img/category-icons/icon-income.svg',
+  'cart': '/img/category-icons/icon-cart.svg',
+  'car': '/img/category-icons/icon-car.svg',
+  'health': '/img/category-icons/icon-health.svg',
+  'clothes': '/img/category-icons/icon-clothes.svg',
+  'fun': '/img/category-icons/icon-fun.svg',
+  'home': '/img/category-icons/icon-home.svg',
+}
